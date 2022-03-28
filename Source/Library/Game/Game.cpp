9d5b74a2e39bdb8b9d76827aeca28589abcf3802
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2022-03-28 20:17:42 +0900

    [LAB02] lab02 submitted

--- Source/Library/Game/Game.cpp
+++ Source/Library/Game/Game.cpp
@@ -2,264 +2,85 @@
 
 namespace library
 {
-    /*--------------------------------------------------------------------
-      Global Variables
-    --------------------------------------------------------------------*/
-    HINSTANCE g_hInstance = NULL;
-    HWND g_hWnd = NULL;
-    D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
-    D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Game::Game
 
-    ComPtr<ID3D11Device> g_pd3dDevice;
-    ComPtr<ID3D11Device1> g_pd3dDevice1;
-    ComPtr<ID3D11DeviceContext> g_pImmediateContext;
-    ComPtr<ID3D11DeviceContext1> g_pImmediateContext1;
-    ComPtr<IDXGISwapChain> g_pSwapChain;
-    ComPtr<IDXGISwapChain1> g_pSwapChain1;
-    ComPtr<ID3D11RenderTargetView> g_pRenderTargetView;
-    /*--------------------------------------------------------------------
-      Forward declarations
-    --------------------------------------------------------------------*/
+	  Summary:  Constructor
 
-    /*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-      Function: WindowProc
+	  Args:     PCWSTR pszGameName
+				  Name of the game
 
-      Summary:  Defines the behavior of the window—its appearance, how
-                it interacts with the user, and so forth
+	  Modifies: [m_pszGameName, m_mainWindow, m_renderer].
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	Game::Game(_In_ PCWSTR pszGameName)
+	{
+		m_pszGameName = pszGameName;
+		m_mainWindow = std::make_unique<MainWindow>();
+		m_renderer = std::make_unique<Renderer>();
+	}
 
-      Args:     HWND hWnd
-                  Handle to the window
-                UINT uMsg
-                  Message code
-                WPARAM wParam
-                  Additional data that pertains to the message
-                LPARAM lParam
-                  Additional data that pertains to the message
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Game::Initialize
 
-      Returns:  LRESULT
-                  Integer value that your program returns to Windows
-    -----------------------------------------------------------------F-F*/
-    LRESULT CALLBACK WindowProc(_In_ HWND hWnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
-    {
-        PAINTSTRUCT ps;
-        HDC hdc;
+	  Summary:  Initializes the components of the game
 
-        switch (uMsg)
-        {
-        case WM_PAINT:
-            hdc = BeginPaint(hWnd, &ps);
-            EndPaint(hWnd, &ps);
-            break;
-        case WM_CLOSE:
-            DestroyWindow(hWnd);
-            return 0;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hWnd, uMsg, wParam, lParam);
-        }
+	  Args:     HINSTANCE hInstance
+				  Handle to the instance
+				INT nCmdShow
+				  Is a flag that says whether the main application window
+				  will be minimized, maximized, or shown normally
 
-        return 0;
-    }
+	  Modifies: [m_mainWindow, m_renderer].
 
-    HRESULT InitWindow(_In_ HINSTANCE hInstance, _In_ INT nCmdShow)
-    {
-        // Registering a Window Class
-        const wchar_t CLASS_NAME[] = L"Game Graphics Programming Lab 01 : Direct3D 11 Basics";
-        WNDCLASSEX wcex =
-        {
-            .cbSize = sizeof(WNDCLASSEX),
-            .style = CS_HREDRAW | CS_VREDRAW,
-            .lpfnWndProc = WindowProc,
-            .cbClsExtra = 0,
-            .cbWndExtra = 0,
-            .hInstance = hInstance,
-            .hIcon = LoadIcon(hInstance, IDI_APPLICATION),
-            .hCursor = LoadCursor(nullptr, IDC_ARROW),
-            .hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1),
-            .lpszMenuName = nullptr,
-            .lpszClassName = CLASS_NAME,
-            .hIconSm = LoadIcon(wcex.hInstance,IDI_APPLICATION)
-        };
+	  Returns:  HRESULT
+				Status code
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	HRESULT Game::Initialize(_In_ HINSTANCE hInstance, _In_ INT nCmdShow)
+	{
+		if (FAILED(m_mainWindow->Initialize(hInstance, nCmdShow, m_pszGameName)))
+			return E_FAIL;
+		if (FAILED(m_renderer->Initialize(m_mainWindow->GetWindow())))
+			return E_FAIL;
+		return S_OK;
+	}
 
-        if (!RegisterClassEx(&wcex))
-        {
-            DWORD dwError = GetLastError();
-            if (dwError != ERROR_CLASS_ALREADY_EXISTS)
-                return HRESULT_FROM_WIN32(dwError);
-            return E_FAIL;
-        }
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Game::Run
 
-        // Creating a Window
-        g_hInstance = hInstance;
-        RECT rc = { 0, 0, 800, 600 };
-        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-        g_hWnd = CreateWindowEx
-        (
-            0,
-            CLASS_NAME,
-            CLASS_NAME,
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-            NULL,
-            NULL,
-            hInstance,
-            NULL
-        );
+	  Summary:  Runs the game loop
 
-        if (!g_hWnd)
-            return E_FAIL;
+	  Returns:  INT
+				  Status code to return to the operating system
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	INT Game::Run()
+	{
+		MSG msg = { 0 };
 
-        // Showing the Window
-        ShowWindow(g_hWnd, nCmdShow);
+		while (WM_QUIT != msg.message)
+		{
+			if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else
+				m_renderer->Render();
+		}
 
-        return S_OK;
-    }
+		return static_cast<INT>(msg.wParam);
+	}
 
-    HRESULT InitDevice()
-    {
-        HRESULT hr = S_OK;
+	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+	  Method:   Game::GetGameName
 
-        RECT rc;
-        GetClientRect(g_hWnd, &rc);
-        UINT width = rc.right - rc.left;
-        UINT height = rc.bottom - rc.top;
+	  Summary:  Returns the name of the game
 
-        UINT createDeviceFlags = 0;
-#ifdef _DEBUG
-        createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
+	  Returns:  PCWSTR
+				  Name of the game
+	M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+	PCWSTR Game::GetGameName() const
+	{
+		return m_pszGameName;
+	}
 
-        D3D_DRIVER_TYPE driverTypes[] =
-        {
-            D3D_DRIVER_TYPE_HARDWARE,
-            D3D_DRIVER_TYPE_WARP,
-            D3D_DRIVER_TYPE_REFERENCE,
-        };
-        UINT numDriverTypes = ARRAYSIZE(driverTypes);
-
-        D3D_FEATURE_LEVEL featureLevels[] =
-        {
-            D3D_FEATURE_LEVEL_11_1,
-            D3D_FEATURE_LEVEL_11_0,
-            D3D_FEATURE_LEVEL_10_1,
-            D3D_FEATURE_LEVEL_10_0,
-        };
-        UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-
-        for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
-        {
-            g_driverType = driverTypes[driverTypeIndex];
-            hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
-                D3D11_SDK_VERSION, g_pd3dDevice.GetAddressOf(), &g_featureLevel, g_pImmediateContext.GetAddressOf());
-
-            if (hr == E_INVALIDARG)
-            {
-                hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
-                    D3D11_SDK_VERSION, g_pd3dDevice.GetAddressOf(), &g_featureLevel, g_pImmediateContext.GetAddressOf());
-            }
-
-            if (SUCCEEDED(hr))
-                break;
-        }
-        if (FAILED(hr))
-            return hr;
-
-        ComPtr<IDXGIFactory1> dxgiFactory;
-        {
-            ComPtr<IDXGIDevice> dxgiDevice;
-            hr = g_pd3dDevice.As(&dxgiDevice);
-
-            if (SUCCEEDED(hr))
-            {
-                ComPtr<IDXGIAdapter> adapter;
-                hr = dxgiDevice->GetAdapter(adapter.GetAddressOf());
-
-                if (SUCCEEDED(hr))
-                    hr = adapter->GetParent(__uuidof(IDXGIFactory1), reinterpret_cast<void**>(dxgiFactory.GetAddressOf()));
-            }
-        }
-        if (FAILED(hr))
-            return hr;
-
-        ComPtr<IDXGIFactory2> dxgiFactory2;
-        hr = dxgiFactory.As(&dxgiFactory2);
-        if (dxgiFactory2)
-        {
-            hr = g_pd3dDevice.As(&g_pd3dDevice1);
-            if (SUCCEEDED(hr))
-                hr = g_pImmediateContext.As(&g_pImmediateContext1);
-
-            DXGI_SWAP_CHAIN_DESC1 sd = {};
-            sd.Width = width;
-            sd.Height = height;
-            sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-            sd.SampleDesc.Count = 1;
-            sd.SampleDesc.Quality = 0;
-            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-            sd.BufferCount = 1;
-
-            hr = dxgiFactory2->CreateSwapChainForHwnd(g_pd3dDevice.Get(), g_hWnd, &sd, nullptr, nullptr, g_pSwapChain1.GetAddressOf());
-            if (SUCCEEDED(hr))
-                hr = g_pSwapChain1.As(&g_pSwapChain);
-        }
-        else
-        {
-            DXGI_SWAP_CHAIN_DESC sd = {};
-            sd.BufferCount = 1;
-            sd.BufferDesc.Width = width;
-            sd.BufferDesc.Height = height;
-            sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-            sd.BufferDesc.RefreshRate.Numerator = 60;
-            sd.BufferDesc.RefreshRate.Denominator = 1;
-            sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-            sd.OutputWindow = g_hWnd;
-            sd.SampleDesc.Count = 1;
-            sd.SampleDesc.Quality = 0;
-            sd.Windowed = TRUE;
-
-            hr = dxgiFactory->CreateSwapChain(g_pd3dDevice.Get(), &sd, g_pSwapChain.GetAddressOf());
-        }
-
-        dxgiFactory->MakeWindowAssociation(g_hWnd, DXGI_MWA_NO_ALT_ENTER);
-
-        if (FAILED(hr))
-            return hr;
-
-        ComPtr<ID3D11Texture2D> pBackBuffer;
-
-        hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(pBackBuffer.GetAddressOf()));
-        if (FAILED(hr))
-            return hr;
-
-        hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, g_pRenderTargetView.GetAddressOf());
-        if (FAILED(hr))
-            return hr;
-
-        g_pImmediateContext->OMSetRenderTargets(1, g_pRenderTargetView.GetAddressOf(), nullptr);
-
-        D3D11_VIEWPORT vp;
-        vp.Width = (FLOAT)width;
-        vp.Height = (FLOAT)height;
-        vp.MinDepth = 0.0f;
-        vp.MaxDepth = 1.0f;
-        vp.TopLeftX = 0;
-        vp.TopLeftY = 0;
-        g_pImmediateContext->RSSetViewports(1, &vp);
-
-        return S_OK;
-    }
-
-    void CleanupDevice()
-    {
-        if (g_pImmediateContext)
-            g_pImmediateContext->ClearState();
-    }
-
-    void Render()
-    {
-        g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView.Get(), Colors::MidnightBlue);
-        g_pSwapChain->Present(0, 0);
-    }
 }
